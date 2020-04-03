513a1c76232dd07b0b2800853b9927bb85c5d3b8
Tyler Rick <tyler@tylerrick.com>
2020-04-02 18:06:34 -0700

    Initial commit

---
+++ lib/rspec/expect_to_make_changes.rb
@@ -0,0 +1,10 @@
+require 'rspec/expect_to_make_changes/version'
+require 'rspec/matchers/make_changes'
+require 'rspec/matchers/before_and_after'
+
+module Rspec
+  module ExpectToMakeChanges
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
