662fb804506e749a927404ced4438f8818bc0fa4
Tyler Rick <tyler@tylerrick.com>
2020-04-08 16:11:26 -0700

    delete

--- lib/rspec/expect_changes.rb
+++
@@ -1,8 +0,0 @@
-require "rspec/expect_changes/version"
-
-module Rspec
-  module ExpectChanges
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- spec/rspec/expect_changes_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Rspec::ExpectChanges do
-  it "has a version number" do
-    expect(Rspec::ExpectChanges::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
